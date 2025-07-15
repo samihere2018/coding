6ffa9e8665c638e3264db0ca1bf804089b2ef7af
Sylvia Amihere <samihere@crimson.ua.edu>
2025-07-15 15:48:54 -0500

    Add files via upload

---
+++ parallel_computing_labs/lab1-intro_unix/file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello, World!";
+
+    return 0;
+}
