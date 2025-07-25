f5498a19ead1611fae09ff7659c5ec86ffde6576
Sylvia Amihere <samihere@crimson.ua.edu>
2025-07-25 13:46:23 -0500

    25th July 2025

---
+++ parallel_computing_labs/parallel_computing_lectEx/driver.f90
@@ -0,0 +1,35 @@
+program Driver
+
+!Declarations
+implicit none
+integer :: n, i, ierr
+double precision, allocatable :: u(:), v(:) 
+double precision :: sum
+
+print*, "Enter the vector length: "
+read*, n
+print*, "          Vector length: ", n
+
+allocate(u(1:n))
+allocate(v(1:n))
+
+!initialise the vectors as zero vectors
+u = 0.d0
+v = 0.d0
+
+!the entries of the vectors
+do i = 1,n
+  u(i) = i
+  v(i) = n - i + 1
+end do
+
+!compute the dot product of the vectors
+sum = 0.d0
+do i = 1,n
+  sum = sum + (u(i)*v(i))
+end do
+
+print*, " Dot product of u and v: ", sum
+
+deallocate(u,v)
+end program Driver
