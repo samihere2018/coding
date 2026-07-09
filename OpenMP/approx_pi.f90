0329eedd605c22e3e9c6e31f0ec300092e41dbb8
Sylvia Amihere <samihere@crimson.ua.edu>
2026-07-08 23:49:43 -0400

    updates

--- OpenMP/approx_pi.f90
+++ OpenMP/approx_pi.f90
@@ -11,7 +11,7 @@ use omp_lib
 implicit none
 
 integer :: i, n, chunksize, tid
-real*8 :: result_pi, delta_x, start_time, end_time, thread_duration, xi, exact_pi
+real*8 :: result_pi, delta_x, total_time, total_start_time, start_time, end_time, thread_duration, xi, exact_pi
 
 exact_pi = dacos(-1.d0)
 chunksize = 10
@@ -26,6 +26,7 @@ elseif (n==0) then
     write(*,*) "PI will be approximated using 10 rectangles."
     n = 10
 end if
+total_start_time = omp_get_wtime()
 
 !$omp parallel default(none) shared(n, chunksize, result_pi, delta_x) private(i, tid, start_time, end_time, thread_duration, xi)
     tid = omp_get_thread_num()
@@ -47,6 +48,7 @@ end if
 write(*,*) "***************************************************************************************************** "
 write(*,*) "The approximate value of PI using ", n, " rectangles: ", result_pi 
 write(*,*) "Absolute error in the approximation of PI using " , n, " rectangles: ", dabs(result_pi - exact_pi)
+write(*,*) "Total time taken to compute the approximation of PI using ", n, " rectangles: ", omp_get_wtime() - total_start_time
 write(*,*)  "***************************************************************************************************** "
 
 end program approx_pi
