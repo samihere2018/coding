824119b09aeeaea0d71634e69de82afeb0242cd6
Sylvia Amihere <samihere@crimson.ua.edu>
2026-07-21 23:13:05 -0400

    updates

---
+++ OpenMP/mean_sd.f90
@@ -0,0 +1,17 @@
+!---Author: Sylvia Amihere 
+!---An OpenMP program in Fortran demonstrating work and sharing of variables among threads
+!---the nowait clause allows threads to continue without waiting for other threads to finish the loop
+!---dynamic scheduling allows threads to pick up the next available chunk of iterations when they finish their current chunk
+!---static scheduling divides the iterations into equal sized chunks and assigns them to threads. Each thread does exactly the same number of iterations. 
+!---Compilation: gfortran -fopenmp -o dummyname workshare2.f90
+!---This program computes the mean adn standard deviation of a set of numbers
+
+program mean_sd
+use omp_lib
+implicit none
+
+
+
+
+
+end program mean_sd
