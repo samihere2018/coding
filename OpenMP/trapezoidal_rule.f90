9caa9efd22de3013c6afa7e56e69cd46e900326e
Sylvia Amihere <samihere@crimson.ua.edu>
2026-07-22 23:23:47 -0400

    updates

---
+++ OpenMP/trapezoidal_rule.f90
@@ -0,0 +1,18 @@
+!---Author: Sylvia Amihere 
+!---An OpenMP program in Fortran demonstrating work and sharing of variables among threads
+!---the nowait clause allows threads to continue without waiting for other threads to finish the loop
+!---dynamic scheduling allows threads to pick up the next available chunk of iterations when they finish their current chunk
+!---static scheduling divides the iterations into equal sized chunks and assigns them to threads. Each thread does exactly the same number of iterations. 
+!---Compilation: gfortran -fopenmp -o dummyname workshare2.f90
+!---This program uses the trapezoidal rule to compute the integral of a function using OpenMP for parallelisation.
+
+program trapezoidal_rule
+use omp_lib
+implicit none
+
+
+
+
+
+
+end program trapezoidal_rule
